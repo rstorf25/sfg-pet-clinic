d7be27cb9ef7f185484b936aa14f53b208ff80ab
rstorf25 <61019008+rstorf25@users.noreply.github.com>
2024-02-02 11:21:37 -0600

    adding POJOs. Closes #2.

---
+++ src/main/java/com/sfgpetclinic/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.sfgpetclinic.sfgpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/sfgpetclinic/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.sfgpetclinic.sfgpetclinic.model;
+
+public class Vet extends Person{
+
+}
