cd3840427b6a5cf9ab19765f16fa855b9bfff9e0
rstorf25 <61019008+rstorf25@users.noreply.github.com>
2024-02-09 15:00:21 -0600

    add bootstrap display list of vets. closes #15.

--- pet-clinic-web/src/main/java/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/sfgpetclinic/controllers/VetController.java
@@ -1,12 +1,24 @@
 package sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sfgpetclinic.service.VetService;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
-    @RequestMapping({"/vets", "/vets/index", "vets/index.html"})
-    public String listVets(){
+
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"","/", "/index", "/index.html"})
+    public String listVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
